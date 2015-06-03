77dd43fe85b7aefdbd179ee7b167dc7c49d26a74
OliviaHihi <olivia.baddeley@gmail.com>
2015-06-03 17:32:15 +1200

    added invisblility functionality

--- invisible/Scripts/invis.js
+++ invisible/Scripts/invis.js
@@ -6,10 +6,29 @@ $(".end").click(function () {
    $(this).css("color", "red"); 
 });
 
-$(".middle").click(function() {
-    $(this).hide();
+//$(".middle").click(function() {
+//    $(this).hide();
+//});
+
+//$(".beginning").click(function() {
+//    $(this).css("background-color", "yellow");
+//})
+
+$(".inviButton").click(function() {
+    $(".message").hide();
 });
 
-$(".beginning").click(function() {
-    $(this).css("background-color", "yellow");
-})
+//$(".showButton").click(function() {
+//    $(".message").show();
+//});
+
+
+//get the value of the textbox input
+$(".showButton").click(function () {
+    console.log($('.codeInput').val() === "SHOW");
+
+    if ($('.codeInput').val() === "SHOW") {
+        $(".message").show();
+        //$('.codeInput').val().replaceWith("");
+    }
+});
