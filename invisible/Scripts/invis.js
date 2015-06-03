eab92e5412168c48335f18e629a51a40f1ff80f3
OliviaHihi <olivia.baddeley@gmail.com>
2015-06-03 16:17:23 +1200

    yellow background and hide on click

--- invisible/Scripts/invis.js
+++ invisible/Scripts/invis.js
@@ -5,3 +5,11 @@
 $(".end").click(function () {
    $(this).css("color", "red"); 
 });
+
+$(".middle").click(function() {
+    $(this).hide();
+});
+
+$(".beginning").click(function() {
+    $(this).css("background-color", "yellow");
+})
