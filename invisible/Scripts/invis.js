9a7a167f3e211612d04ea55ebe23c4f5d73f7daf
OliviaHihi <olivia.baddeley@gmail.com>
2015-09-10 12:32:08 +1200

    updated look and the clearing secret word text box after submission

--- invisible/Scripts/invis.js
+++ invisible/Scripts/invis.js
@@ -25,12 +25,26 @@ $(".inviButton").click(function() {
 //});
 
 
+
+
+
 //get the value of the textbox input
 $(".showButton").click(function () {
-    console.log($('.codeInput').val() === "SHOW");
+    //console.log($('.codeInput').val() === "SHOW");
+    console.log($('.codeInput').val());
+    console.log(this);
 
     if ($('.codeInput').val() === "SHOW") {
         $(".message").show();
         //$('.codeInput').val().replaceWith(""); //to clear the textbox input..
+        //$('.codeInput').value = "";
+        //console.log($('.codeInput').val());
+        $('.codeInput').val("");
+       // console.log($('.codeInput').val());
+    }
+    else {
+        $('.codeInput').val("Try again");
     }
 });
+
+
